be9ef3dd8ba24e9fbc96276e84a821071a2bee69
amennajam009 <amennajam7@gmail.com>
2023-09-07 22:48:20 +0500

    add

--- RestOperator.js
+++ RestOperator.js
@@ -10,3 +10,10 @@ Myvalues(10)
 
 
 //New method to pass arguments
+
+function MynewValues(...values){
+    for(let i=1; i<values;i++){
+        console.log(i)
+    }
+}
+console.log(MynewValues(20))
